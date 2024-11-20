44faba69b3101b88a181dce3046803feed741a45
AndrejYaotsin <andrej.butalov@gmx.de>
2024-11-20 11:41:09 +0100

    header

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -3,13 +3,13 @@ import { AboutMeComponent } from './about-me/about-me.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ReferencesComponent } from './references/references.component';
-import { TitleComponent } from './title/title.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
   imports: [
-    TitleComponent,
+    HeaderComponent,
     AboutMeComponent,
     MySkillsComponent,
     PortfolioComponent,
