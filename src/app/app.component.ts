afdef3d07a196ba4a90dab8ec4eaa88a7d896ef1
AndrejYaotsin <andrej.butalov@gmx.de>
2024-11-20 10:53:58 +0100

    main_conten/footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule, 
+    RouterOutlet, 
+    FooterComponent, 
+    NavbarComponent,
+    MainContentComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
