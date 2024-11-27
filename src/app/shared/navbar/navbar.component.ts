74bb7a32dfb4e34b8eaae3c08c6839b42b0effc1
AndrejYaotsin <andrej.butalov@gmx.de>
2024-11-27 12:18:56 +0100

    menu links to content

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,15 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router, NavigationEnd } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit, OnDestroy {
+  menuActive = false;
+  private routerSubscription: Subscription | null = null;
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.routerSubscription = this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.menuActive = event.url === '/menu';
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.routerSubscription) {
+      this.routerSubscription.unsubscribe();
+    }
+  }
+
+  toggleMenu() {
+    this.menuActive = !this.menuActive;
+    if (this.menuActive) {
+      this.router.navigate(['/menu']);
+    } else {
+      this.router.navigate(['/']);
+    }
+  }
 }
