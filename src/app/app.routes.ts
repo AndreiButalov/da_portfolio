ea25b5da0862a997f1ea480b916bbf26e4e8c261
AndrejYaotsin <andrej.butalov@gmx.de>
2024-12-03 09:21:39 +0100

    add privacy policy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
 import { MenuContentComponent } from './menu-content/menu-content.component';
 import { ImprintComponent } from './imprint/imprint.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
     { path: '', component: MainContentComponent },
     { path: 'menu', component: MenuContentComponent },
     { path: 'imprint', component: ImprintComponent },
+    { path: 'privacy_policy', component: PrivacyPolicyComponent },
 ];
--- src/app/main-content/references/references.component.ts
+++ src/app/main-content/references/references.component.ts
@@ -16,21 +16,21 @@ export class ReferencesComponent {
   references = [
     {
       image: '/assets/img/format_quote.png',
-      name: 'Andrei',
+      name: 'Team Partner 1',
       status: 'Team Partner',
       description: "Andrei really kept the togenther with his great organisation and clear communication. We wouldn't have got this far without his commitment."
     },
     {
       image: '/assets/img/format_quote.png',
-      name: 'Andrei2',
+      name: 'Team Partner 2',
       status: 'Team Partner',
-      description: "Andrei really kept the togenther with his great organisation and clear communication. We wouldn't have got this far without his commitment."
+      description: "Andrei was a top team colleague at DA. His positive commitment and willingness to take on responsibility made a singnificant contribution tu us achieving our goals."
     },
     {
       image: '/assets/img/format_quote.png',
-      name: 'Andrei3',
+      name: 'Team Partner 3',
       status: 'Team Partner',
-      description: "Andrei really kept the togenther with his great organisation and clear communication. We wouldn't have got this far without his commitment. Andrei really kept the togenther with his great organisation and clear "
+      description: "It was a great pleasure to work with Andrei. He knows how to push and encourage team members to present the best work possible, always adding something to brainstorm."
     },
   ]
 
