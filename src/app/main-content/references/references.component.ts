1a7840d35454d7bad31c1185a11bc93bea3ff23f
AndrejYaotsin <andrej.butalov@gmx.de>
2024-12-11 11:23:13 +0100

    references in germany

--- src/app/main-content/references/references.component.ts
+++ src/app/main-content/references/references.component.ts
@@ -28,22 +28,33 @@ export class ReferencesComponent {
       image: 'assets/img/format_quote.png',
       name: 'Team Partner 1',
       status: 'Team Partner',
-      description: "Andrei really kept the togenther with his great organisation and clear communication. We wouldn't have got this far without his commitment.",
-      descriptionDe: "Andrei hat das Team mit seiner großartigen Organisation und klarer Kommunikation zusammengehalten. Ohne sein Engagement wären wir nicht so weit gekommen."
+      description: `Andrei really kept the togenther with his great organisation
+      and clear communication. We wouldn't have got this far without his commitment.`,
+      descriptionDe: `Andrei hat das Team mit seiner großartigen Organisation und
+      klarer Kommunikation zusammengehalten. Ohne sein Engagement wären wir nicht 
+      so weit gekommen.`
     },
     {
       image: 'assets/img/format_quote.png',
       name: 'Team Partner 2',
       status: 'Team Partner',
-      description: "Andrei was a top team colleague at DA. His positive commitment and willingness to take on responsibility made a singnificant contribution tu us achieving our goals.",
-      descriptionDe: "Andrei war ein hervorragender Teamkollege bei DA. Sein positives Engagement und die Bereitschaft, Verantwortung zu übernehmen, haben maßgeblich dazu beigetragen, dass wir unsere Ziele erreicht haben."
+      description: `Andrei was a top team colleague at DA. His positive commitment 
+      and willingness to take on responsibility made a singnificant contribution 
+      tu us achieving our goals.`,
+      descriptionDe: `Andrei war ein hervorragender Teamkollege bei DA. Sein 
+      positives Engagement und die Bereitschaft, Verantwortung zu übernehmen, haben 
+      maßgeblich dazu beigetragen, dass wir unsere Ziele erreicht haben.`
     },
     {
       image: 'assets/img/format_quote.png',
       name: 'Team Partner 3',
       status: 'Team Partner',
-      description: "It was a great pleasure to work with Andrei. He knows how to push and encourage team members to present the best work possible, always adding something to brainstorm.",
-      descriptionDe: "Es war eine große Freude, mit Andrei zu arbeiten. Er weiß, wie man Teammitglieder motiviert und dazu anregt, die besten Ergebnisse zu präsentieren, und trägt immer etwas zur Ideensammlung bei."
+      description: `It was a great pleasure to work with Andrei. He knows how 
+      to push and encourage team members to present the best work possible, 
+      always adding something to brainstorm.`,
+      descriptionDe: `Es war eine große Freude, mit Andrei zu arbeiten. 
+      Er weiß, wie man Teammitglieder motiviert und dazu anregt, die besten 
+      Ergebnisse zu präsentieren, und trägt immer etwas zur Ideensammlung bei.`
     },
   ] 
     
