b1910983ecb3a02290800f99665de28509b9bfdc
AndrejYaotsin <andrej.butalov@gmx.de>
2024-12-16 14:03:01 +0100

    about_me, myskills, porfolio links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,15 @@ import { MainContentComponent } from './main-content/main-content.component';
 import { MenuContentComponent } from './menu-content/menu-content.component';
 import { ImprintComponent } from './imprint/imprint.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { AboutMeComponent } from './main-content/about-me/about-me.component';
+import { MySkillsComponent } from './main-content/my-skills/my-skills.component';
+import { PortfolioComponent } from './main-content/portfolio/portfolio.component';
 
 export const routes: Routes = [
     { path: '', component: MainContentComponent },
+    { path: 'about_me', component: AboutMeComponent },
+    { path: 'my_skills', component: MySkillsComponent },
+    { path: 'portfolio', component: PortfolioComponent },
     { path: 'menu', component: MenuContentComponent },
     { path: 'imprint', component: ImprintComponent },
     { path: 'privacy_policy', component: PrivacyPolicyComponent },
